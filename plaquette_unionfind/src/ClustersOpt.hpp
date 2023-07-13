96f2e693b3dddff20b511885d93b2e863958cdfa
Trevor Vincent <trevor.j.vincent@gmail.com>
2023-07-13 04:07:16 -0400

    Test passes

--- plaquette_unionfind/src/ClustersOpt.hpp
+++ plaquette_unionfind/src/ClustersOpt.hpp
@@ -11,6 +11,17 @@
 
 namespace Plaquette {
 
+  struct Sizes {
+    std::vector<size_t> boundary_sizes; // size of the cluster boundary (in # of vertices)
+    std::vector<float> cluster_sizes; // size of the cluster (sum over all edge lengths)
+    std::vector<size_t> erasure_sizes; // size of the erasure in each cluster (sum over all fully grown edges);
+  };
+
+  enum class CompareSizeMethod {
+    BOUNDARY_SIZE_THEN_CLUSTER_SIZE,
+    BOUNDARY_SIZE_THEN_ERASURE_SIZE
+  };
+  
 /**
  * @brief Represents a set of clusters used in decoding a quantum
  * error-correcting code.
@@ -32,7 +43,8 @@ private:
 
   std::vector<int> vertex_to_cluster_id_;
   std::vector<bool> syndrome_;
-
+  Sizes sizes_;
+  
 public:
   /**
    * @brief Constructs a new `Clusters`.
@@ -70,10 +82,16 @@ public:
     vertex_to_cluster_id_ =
         std::vector<int>(decoding_graph.GetNumVertices(), -1);
 
+    // for (size_t i = 0; i < cluster_parity_.size(); i++){
+    //   cluster_parity_[i] = decoding_graph.IsVertexOnBoundary(i) ? -1 : 0;
+    // }
+    
     InitClusterRoots_(syndrome);
   }
 
   auto GetEdgeGrowth() const { return edge_growth_; }
+  auto GetVertexToClusterId() const { return vertex_to_cluster_id_; }
+  auto GetClusterParity() const { return cluster_parity_; }
 
   /**
    * @brief Initialize the roots of the clusters.
@@ -126,60 +144,151 @@ public:
     }
   }
 
-  inline std::vector<size_t> FindEdgesToFuse() const {
+  std::vector<int> FindNewFullyGrownEdges() {
     std::vector<size_t> edges_to_fuse(decoding_graph_.GetNumEdges());
+    std::vector<int> vertices_to_fuse(decoding_graph_.GetNumVertices(),-1);
     
     for (size_t edge_id = 0; edge_id < decoding_graph_.GetNumEdges(); edge_id++) {
-      std::pair<size_t, size_t> local_edges = edge_id_to_local_edges_[edge_id];
+      auto local_edge_left = decoding_graph_.GetLocalEdgeFromGlobalEdge(edge_id,0);
+      auto local_edge_right = decoding_graph_.GetLocalEdgeFromGlobalEdge(edge_id,1);
       edges_to_fuse[edge_id] =
           !fully_grown_edges_[edge_id] and
-          (edge_growth_[local_edges.first] + edge_growth_[local_edges.second] >=
+          (edge_growth_[local_edge_left] + edge_growth_[local_edge_right] >=
            max_growth_);
+      fully_grown_edges_[edge_id] = edges_to_fuse[edge_id] or fully_grown_edges_[edge_id];
     }
     
     for (size_t edge_id = 0; edge_id < decoding_graph_.GetNumEdges();
          edge_id++) {
       if (edges_to_fuse[edge_id]) {
         std::pair<size_t, size_t> vertices =
-            decoding_graph_.GetVerticesTouchingEdge(edge_id);
+            decoding_graph_.GetVerticesConnectedByEdge(edge_id);
         int cluster_root = std::max({vertex_to_cluster_id_[vertices.first],
                                      vertex_to_cluster_id_[vertices.second]});
         vertices_to_fuse[vertices.first] =
             vertices_to_fuse[cluster_root] == -1
                 ? cluster_root
-                : vertices_to_fuse[vertices.first];
+                : vertices_to_fuse[cluster_root];
+	
         vertices_to_fuse[vertices.second] =
             vertices_to_fuse[cluster_root] == -1
                 ? cluster_root
-                : vertices_to_fuse[vertices.first];
+                : vertices_to_fuse[cluster_root];
       }
     }
     return vertices_to_fuse;
   }
 
-  inline void FuseEdges(const std::vector<size_t> &vertices_to_fuse) {
+  inline void FuseEdges(const std::vector<int> &vertices_to_fuse) {
     for (size_t vertex_id = 0; vertex_id < decoding_graph_.GetNumVertices();
          vertex_id++) {
-      vertex_to_cluster_id_[vertex_id] = (vertices_to_fuse[vertex_id] != -1)
-	? vertices_to_fuse[vertex_id]
-	: vertex_to_cluster_id_[vertex_id];
+      if (vertices_to_fuse[vertex_id] != -1) {
+	vertex_to_cluster_id_[vertex_id] = vertices_to_fuse[vertex_id];
+      }
+      else {
+	if (vertices_to_fuse[vertex_to_cluster_id_[vertex_id]] != -1) {
+	  vertex_to_cluster_id_[vertex_id] = vertices_to_fuse[vertex_to_cluster_id_[vertex_id]];
+	}
+      }
     }
   }
 
-  inline UpdateClusterParity() {
-    for (size_t vertex_id = 0; vertex_id < decoding_graph_.GetNumVertices();
-         vertex_id++) {
-
-      cluster_parity_[vertex_to_cluster_id_[vertex_id]] =
-	(syndrome_[vertex_id] == -1 or
-	 cluster_parity_[vertex_to_cluster_id_[vertex_id]] == -1)
-	? -1
-	: cluster_parity_[vertex_to_cluster_id_[vertex_id]] +
-	syndrome_[vertex_id];
-      
-    }
+  inline int AddParity(int parity_a, int parity_b) {
+    return (parity_a < 0 or parity_b < 0) ? -1 : (parity_a + parity_b) == 2 ? 0 : (parity_a + parity_b);
   }
 
+  inline bool UpdateClusterParity() {
+    size_t sum = 0;
+    for (size_t vertex_id = 0; vertex_id < decoding_graph_.GetNumVertices();
+         vertex_id++) {
+      cluster_parity_[vertex_to_cluster_id_[vertex_id]] = decoding_graph_.IsVertexOnBoundary(vertex_id) ? -1 : AddParity(cluster_parity_[vertex_to_cluster_id_[vertex_id]],syndrome_[vertex_id]);
+      cluster_parity_[vertex_id] = (vertex_id == vertex_to_cluster_id_[vertex_id]) ? cluster_parity_[vertex_id] : 0;
+      sum += (cluster_parity_[vertex_id] == 1);
+    }
+    return sum != 0;
+  }
+
+
+  template <typename Code>
+  auto GetVisualizer(Code &code, const StabilizerCode::GridType &grid_type,
+		     bool annotate = false) const {
+
+    auto lv = code.GetVisualizer(grid_type, annotate);
+    const auto &coords = grid_type == StabilizerCode::GridType::Z
+      ? code.GetZStabilizerCoords()
+      : code.GetXStabilizerCoords();
+
+    size_t k = 0;
+    for (size_t cc = 0; cc < decoding_graph_.GetNumVertices(); cc++) {
+
+      if (vertex_to_cluster_id_[cc] != cc)
+	continue;
+
+      size_t c = cc;
+
+      std::string cluster_color = GetHexColor(k);
+      VertexPrintProps vpp;
+      vpp.vertex = coords[c];
+      vpp.marker = "x";
+      vpp.annotation = "";
+      vpp.color = cluster_color;
+      vpp.markersize = 20;
+      vpp.fillstyle = "full";
+      vpp.label = std::to_string(c) + "_root";
+      lv.AddVertexProps(vpp);
+
+      for (size_t v = 0; v < decoding_graph_.GetNumVertices(); ++v) {
+
+	if (vertex_to_cluster_id_[v] == c) {
+	  size_t v_stride = decoding_graph_.GetLocalEdgeStride(v);
+
+	  if (vertex_to_cluster_id_[v] == c and v != c) {
+	    VertexPrintProps vpp;
+	    vpp.vertex = coords[v];
+	    vpp.marker = "o";
+	    vpp.annotation = "";
+	    vpp.color = cluster_color;
+	    vpp.markersize = 10;
+	    vpp.fillstyle = "full";
+	    vpp.label = std::to_string(c) + "_vertex";
+	    lv.AddVertexProps(vpp);
+	  }
+
+	  const auto &edges =
+	    decoding_graph_.GetEdgesTouchingVertex(v);
+	  const auto &vertices =
+	    decoding_graph_.GetVerticesTouchingVertex(v);
+
+	  for (size_t e = 0; e < edges.size(); ++e) {
+
+	    float edge_growth = edge_growth_[v_stride + e];
+	    if (edge_growth > 0) {
+	      EdgePrintProps epp;
+	      epp.vertex_0 = coords[v];
+	      epp.vertex_1 = coords[vertices[e]];
+	      if (code.IsPeriodic()) {
+		code.FixEdgeCoordsForVisual(epp.vertex_0,
+					    epp.vertex_1);
+	      }
+	      epp.linestyle = "-";
+	      epp.linewidth = 20;
+	      epp.color = cluster_color;
+	      epp.label = std::to_string(c) + "_edge";
+	      epp.alpha = 1.0;
+	      epp.fraction = (edge_growth > 2.0) ? 1.0 : edge_growth / 2.0;
+	      epp.annotation = "";
+	      lv.AddEdgeProps(epp);
+	    }
+	  }
+	}
+      }
+      k++;
+    }
+    return lv;
+  };
+
+
+  
   // inline std::vector<size_t> GetFuseList(){
   //   std::vector<size_t> fuse_list(num_vertices);
   //   for (size_t i = 0; i < possible_global_edges_to_fuse.size(); i++){
--- plaquette_unionfind/src/tests/Test_ClusterOpt.hpp
+++ plaquette_unionfind/src/tests/Test_ClusterOpt.hpp
@@ -33,17 +33,50 @@ TEST_CASE("ClustersOpt GrowCluster returns the correct possible edges to fuse") 
       auto dg = code.GetZStabilizerDecodingGraph();
       std::vector<bool> syndrome(dg.GetNumVertices(), false);
       syndrome[1] = true;
-      syndrome[5] = true;
+      syndrome[4] = true;
       syndrome[7] = true;
-      ClustersOpt cluster_set(dg, syndrome, {},
-			    {});
+      ClustersOpt cluster_set(dg, syndrome, {}, {});
 
+      auto cluster_parity = cluster_set.GetClusterParity();
+      for (size_t i = 0; i < cluster_parity.size(); i++) {
+	std::cout << "parity = " << cluster_parity[i] << std::endl;
+      }
+
+      
       cluster_set.GrowClusters();
       auto local_edges = cluster_set.GetEdgeGrowth();
+      
       for (size_t i = 0; i < local_edges.size(); i++) {
-	std::cout << local_edges[i] << std::endl;
+	auto global_edge = dg.GetGlobalEdgeFromLocalEdge(local_edges[i]);
+	auto vertices = dg.GetVerticesConnectedByEdge(global_edge);
+	std::cout << i << " " << local_edges[i] << std::endl;
       }
+
+      // LatticeVisualizerDB db;
+      // db.AddVisualizer("before_fuse",cluster_set.GetVisualizer(code, StabilizerCode::GridType::Z, true));
+      // db.Plot("before_fuse");
+
       // auto vertices_to_fuse = FindEdgesToFuse();
+      auto vertices_to_fuse = cluster_set.FindNewFullyGrownEdges();
+      cluster_set.FuseEdges(vertices_to_fuse);
+      for (size_t i = 0; i < vertices_to_fuse.size(); i++) {
+	std::cout << i << " " << vertices_to_fuse[i] << std::endl;
+      }
+
+      auto vertex_to_cluster_id = cluster_set.GetVertexToClusterId();
+      for (size_t i = 0; i < vertex_to_cluster_id.size(); i++) {
+	std::cout << i << " " << vertex_to_cluster_id[i] << std::endl;
+      }
+
+      cluster_set.UpdateClusterParity();
+      cluster_parity = cluster_set.GetClusterParity();
+
+      for (size_t i = 0; i < cluster_parity.size(); i++) {
+	REQUIE(cluster_parity[i] == 0);
+      }
+      
+      // db.AddVisualizer("after_fuse",cluster_set.GetVisualizer(code, StabilizerCode::GridType::Z, true));
+      // db.Plot("after_fuse");
       
     }
     
